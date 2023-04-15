ea219377b578acf506bae95e219b868dd4646434
IdanCo <idan@idancohen.com>
2023-04-15 17:56:07 +0300

    add angular fire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'has-ai-taken-over',
+    appId: '1:424633508461:web:2d5066f6f74f3568c31564',
+    storageBucket: 'has-ai-taken-over.appspot.com',
+    apiKey: 'AIzaSyATuP_Q-Bei04-Gm62YQbug566t28iaxFM',
+    authDomain: 'has-ai-taken-over.firebaseapp.com',
+    messagingSenderId: '424633508461',
+  },};
