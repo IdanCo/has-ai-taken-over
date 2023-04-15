68d56d5f5992bca5f1163f22a88e6341bce11b22
IdanCo <idan@idancohen.com>
2023-04-15 20:11:14 +0300

    basic flow

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,24 +1,32 @@
 import * as functions from "firebase-functions";
+import axios from "axios";
+import { Configuration, OpenAIApi } from 'openai';
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+export const addNumbers = functions
+  .runWith({ secrets: ["OPANAI_KEY", 'NEWSAPI_KEY'] })
+  .https.onCall(async (data, context) => {
+    // Fetch the latest AI headlines
+    const newsAPIKey = process.env.NEWSAPI_KEY;
+    const newsAPIUrl = `https://newsapi.org/v2/everything?q=AI%20advancement&sortBy=publishedAt&apiKey=${newsAPIKey}`;
+    const newsResponse = await axios.get(newsAPIUrl);
+    const headlines = newsResponse.data.articles.map((article: any) => article.title).join('. ');
 
-export const addNumbers = functions.https.onCall((data, context) => {
-  const { number1, number2 } = data;
+    const configuration = new Configuration({ apiKey: process.env.OPANAI_KEY });
+    const openaiApi = new OpenAIApi(configuration);
 
-  if (typeof number1 !== 'number' || typeof number2 !== 'number') {
-    throw new functions.https.HttpsError(
-      'invalid-argument',
-      'Both number1 and number2 must be numbers.'
-    );
-  }
+    console.info(1111, process.env.OPANAI_KEY);
+
+    // Send the headlines to OpenAI API
+    const aiResponse = await openaiApi.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      max_tokens: 50,
+      n: 1,
+      temperature: 0.7,
+      messages: [{role: "user", content: `Analyze the following headlines regarding AI advancement: "${headlines}". Has AI taken over the world? Provide an answer in the following format: { result: boolean; reasoning: string `}],
+    });
 
   return {
-    result: number1 + number2,
+    headlines: headlines,
+    aiResponse: aiResponse.data,
   };
 });
