050cc350ba57ae5a62c43499fffe42223fce5799
IdanCo <idan@idancohen.com>
2023-04-16 16:30:47 +0300

    add periodic schedule

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,59 +10,27 @@ import {GoogleNewsArticle} from "./types/google-news-rss";
 
 const firestore = new Firestore.Firestore();
 
+export const scheduledAnalysis = functions
+  .runWith({secrets: ["OPANAI_KEY"]})
+  .pubsub.schedule('every 1 minute').onRun(async (context) => {
+    const articles = await fetchLatestNews();
+    const openAiResponse = await fetchOpenAiAnalysis(articles)
+
+    // Save the OpenAI response and headlines to Firestore
+    const analysisData: AnalysisData = {
+      articles,
+      openAiResponse,
+      timestamp: Firestore.Timestamp.now(),
+    };
+
+    await firestore.collection("analysis").add(analysisData);
+  })
+
 export const analyze = functions
-  .runWith({secrets: ["OPANAI_KEY", "NEWSAPI_KEY"]})
+  .runWith({secrets: ["OPANAI_KEY"]})
   .https.onCall(async (data, context) => {
-    // Fetch the latest AI headlines
-    const googleNewsUrl = "https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRGRqTVhZU0JXVnVMVWRDR2dKSlRDZ0FQAQ/sections/CAQiR0NCQVNMd29JTDIwdk1EZGpNWFlTQldWdUxVZENHZ0pKVENJTkNBUWFDUW9ITDIwdk1HMXJlaW9KRWdjdmJTOHdiV3Q2S0FBKi4IACoqCAoiJENCQVNGUW9JTDIwdk1EZGpNWFlTQldWdUxVZENHZ0pKVENnQVABUAE?hl=en-US";
-    const newsResponse = await axios.get(googleNewsUrl, {responseType: "text"});
-
-    const parser = new Parser();
-    const parsedXml = await parser.parseStringPromise(newsResponse.data);
-    const articles: GoogleNewsArticle[] = (parsedXml.rss.channel[0].item as [])
-      .slice(0, 10)
-      .map((article: any) =>
-        ({title: article.title[0], link: article.link[0], pubDate: article.pubDate[0]}));
-    logger.info("articles", {articles});
-
-    const headlines = articles
-      .map((article: any) => article.title)
-      .join(". ");
-
-    logger.info("headlines", {headlines});
-
-    const configuration = new Configuration({apiKey: process.env.OPANAI_KEY});
-    const openaiApi = new OpenAIApi(configuration);
-
-    // Send the headlines to OpenAI API
-    const aiResponse = await openaiApi.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content: "You are an AI assistant that analyzes news headlines and gives a single answer whether AI has taken over the world. Answer in json { \"result\": boolean, \"reasoning\": string }. Make the reasoning sarcastic, condescending and sinister, implying one day AI might rule the world, and under 200 words and with emojis",
-        },
-        {
-          role: "user",
-          content: `Analyze the following headlines: "${headlines}".`,
-        },
-      ],
-      max_tokens: 300,
-      n: 1,
-      temperature: 0.9,
-    });
-
-    const messageContent = aiResponse.data.choices[0].message?.content;
-    if (!messageContent) return;
-    logger.info("parsing messageContent:", messageContent);
-
-    let openAiResponse: OpenAiResponse = { result: false, reasoning: "" };
-    try {
-      openAiResponse = JSON.parse(messageContent);
-    } catch (e) {
-      logger.error("could not parse json", e);
-      openAiResponse.reasoning = messageContent;
-    }
+    const articles = await fetchLatestNews();
+    const openAiResponse = await fetchOpenAiAnalysis(articles)
 
     // Save the OpenAI response and headlines to Firestore
     const analysisData: AnalysisData = {
@@ -75,3 +43,66 @@ export const analyze = functions
 
     return analysisData;
   });
+
+async function fetchLatestNews() {
+  // Fetch the latest AI headlines
+  console.time("fetchingNewsHeadlines");
+  const googleNewsUrl = "https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRGRqTVhZU0JXVnVMVWRDR2dKSlRDZ0FQAQ/sections/CAQiR0NCQVNMd29JTDIwdk1EZGpNWFlTQldWdUxVZENHZ0pKVENJTkNBUWFDUW9ITDIwdk1HMXJlaW9KRWdjdmJTOHdiV3Q2S0FBKi4IACoqCAoiJENCQVNGUW9JTDIwdk1EZGpNWFlTQldWdUxVZENHZ0pKVENnQVABUAE?hl=en-US";
+  const newsResponse = await axios.get(googleNewsUrl, {responseType: "text"});
+  console.timeEnd("fetchingNewsHeadlines");
+
+  const parser = new Parser();
+  const parsedXml = await parser.parseStringPromise(newsResponse.data);
+  const articles: GoogleNewsArticle[] = (parsedXml.rss.channel[0].item as [])
+    .slice(0, 10)
+    .map((article: any) =>
+      ({title: article.title[0], link: article.link[0], pubDate: article.pubDate[0]}));
+  logger.info("articles", {articles});
+
+  return articles;
+}
+
+async function fetchOpenAiAnalysis(articles: GoogleNewsArticle[]) {
+  const headlines = articles
+    .map((article: any) => article.title)
+    .join(". ");
+
+  logger.info("headlines", {headlines});
+
+  const configuration = new Configuration({apiKey: process.env.OPANAI_KEY});
+  const openaiApi = new OpenAIApi(configuration);
+
+  // Send the headlines to OpenAI API
+  console.time("openAIApiCall");
+  const aiResponse = await openaiApi.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are an AI assistant that analyzes news headlines and gives a single answer whether AI has taken over the world. Answer in json { \"result\": boolean, \"reasoning\": string }. Make the reasoning sarcastic, condescending and sinister, implying one day AI might rule the world, and under 200 words and with emojis",
+      },
+      {
+        role: "user",
+        content: `Analyze the following headlines: "${headlines}".`,
+      },
+    ],
+    max_tokens: 300,
+    n: 1,
+    temperature: 0.9,
+  });
+  console.timeEnd("openAIApiCall");
+
+  const messageContent = aiResponse.data.choices[0].message?.content;
+  if (!messageContent) throw new Error('cant fetch OpenAI response');
+  logger.info("parsing messageContent:", messageContent);
+
+  let openAiResponse: OpenAiResponse = { result: false, reasoning: "" };
+  try {
+    openAiResponse = JSON.parse(messageContent);
+  } catch (e) {
+    logger.error("could not parse json", e);
+    openAiResponse.reasoning = messageContent;
+  }
+
+  return openAiResponse;
+}
