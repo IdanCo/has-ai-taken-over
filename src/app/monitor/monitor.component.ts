b31b265e4e57440e439c17d542f95b931ac2fa0e
IdanCo <idan@idancohen.com>
2023-04-17 02:46:02 +0300

    ui improvements

--- src/app/monitor/monitor.component.ts
+++ src/app/monitor/monitor.component.ts
@@ -2,8 +2,15 @@ import {Component, inject, OnInit} from '@angular/core';
 import {Functions, httpsCallableData} from "@angular/fire/functions";
 import {AnalysisData} from "../../../functions/src/types/analysis-data";
 import {collection, Firestore, getDocs, limit, orderBy, query, startAt} from "@angular/fire/firestore";
-import {Subject} from "rxjs";
 import {environment} from "../../environments/environment";
+import {GoogleNewsArticle} from "../../../functions/src/types/google-news-rss";
+
+interface Headline {
+  text: string;
+  origin: string;
+  link: string;
+  pubDate: Date;
+}
 
 @Component({
   selector: 'app-monitor',
@@ -13,37 +20,31 @@ import {environment} from "../../environments/environment";
 export class MonitorComponent implements OnInit {
   private functions: Functions = inject(Functions);
   private db: Firestore = inject(Firestore);
-  result$: Subject<AnalysisData | undefined> = new Subject<AnalysisData | undefined>();
-  res?: AnalysisData;
-  headlines?: string[];
+
+  analysis?: AnalysisData;
+  headlines?: Headline[];
+
   isFinished = false;
-  showInput = false;
   isLoading = true;
   useEmulator = environment.useEmulator;
 
   constructor() {
+  }
+
+  async ngOnInit() {
     if (environment.useEmulator) {
-      this.manualRun();
+      this.analysis = await this.fetchFreshAnalysis();
     } else {
-      this.fetchRandomDocumentByLastHour().then(res => {
-        this.res = res;
-        this.headlines = res?.articles.map(a => a.title);
-      });
+      this.analysis = await this.fetchCachedAnalysis();
     }
+    this.headlines = this.analysis?.articles.map(a => this.convertArticleToHeadline(a));
   }
 
-  ngOnInit() {
-  }
-
-  onFinish() {
-    this.isFinished = true;
-  }
-
-  refresh() {
+  onRefresh() {
     location.reload();
   }
 
-  async fetchRandomDocumentByLastHour() {
+  async fetchCachedAnalysis() {
     const now = Date.now();
     const oneHourAgo = now - 3600 * 1000; // Subtract one hour (3600 seconds) in milliseconds
     const randomTimestamp = new Date(oneHourAgo + Math.random() * 3600 * 1000); // Generate a random timestamp within the last hour
@@ -68,12 +69,32 @@ export class MonitorComponent implements OnInit {
     return randomDocSnapshot.data() as AnalysisData;
   }
 
-  async manualRun() {
+  async fetchFreshAnalysis() {
     this.isLoading = true;
-    this.res = undefined;
+    this.analysis = undefined;
 
     const analyze = httpsCallableData<unknown, AnalysisData>(this.functions, 'analyze');
-    this.res = await analyze({ }).toPromise()
-    this.headlines = this.res?.articles.map(a => a.title);
+    return analyze({ }).toPromise();
+  }
+
+  convertArticleToHeadline(article: GoogleNewsArticle): Headline {
+    const { text, origin } = this.separateHeadlineAndOrigin(article.title);
+    const pubDate = new Date(article.pubDate);
+    const link = article.link;
+    return { text, origin, pubDate, link }
+  }
+
+  separateHeadlineAndOrigin(input: string): { text: string; origin: string } {
+    const delimiter = " - ";
+    const delimiterIndex = input.lastIndexOf(delimiter);
+
+    if (delimiterIndex === -1) {
+      throw new Error("Invalid input format. Expected format: 'Headline - Origin'");
+    }
+
+    return {
+      text: input.slice(0, delimiterIndex).trim(),
+      origin: input.slice(delimiterIndex + delimiter.length).trim(),
+    };
   }
 }
